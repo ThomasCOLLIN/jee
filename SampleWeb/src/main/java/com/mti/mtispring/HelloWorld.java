8c9ee84edcfde132b402e663fda272e8bf382c1a
Thomas COLLIN <thmcollin@gmail.com>
2013-07-01 18:08:17 +0200

    BUUUUUUUUUUUUG

--- SampleWeb/src/main/java/com/mti/mtispring/HelloWorld.java
+++ SampleWeb/src/main/java/com/mti/mtispring/HelloWorld.java
@@ -4,7 +4,11 @@
  */
 package com.mti.mtispring;
 
+import com.mti.mtispring.db.daos.PeopleDAO;
+import com.mti.mtispring.db.entities.People;
+import java.util.List;
 import javax.jws.WebService;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -16,7 +20,11 @@ public class HelloWorld implements HelloService {
     /**
      * This is a sample web service operation
      */
+    @Override
+    @Transactional(readOnly=true)
     public String hello(String txt) {
-        return "Hello " + txt + " !";
+        PeopleDAO dao = new PeopleDAO();
+        List<People> list = dao.findAll();
+        return list.get(0).toString();
     }
 }
--- SampleWeb/src/main/java/com/mti/mtispring/db/daos/PeopleDAO.java
+++ SampleWeb/src/main/java/com/mti/mtispring/db/daos/PeopleDAO.java
@@ -6,30 +6,53 @@ package com.mti.mtispring.db.daos;
 
 import com.mti.mtispring.db.entities.People;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
  * @author collin_t
  */
-public class PeopleDAO implements IsDAO<People> {
-
+public class PeopleDAO implements IsDAO<People> 
+{
+    @PersistenceContext
+    private EntityManager entityManager;
+    
     @Override
-    public void persist(People t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void persist(People t) 
+    {
+//        emf.persist(t);
+//        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void remove(People t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void remove(People t) 
+    {
+//        entityManager.remove(t);
+//        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void merge(People t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void merge(People t) 
+    {
+//        entityManager.merge(t);
+//        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public List<People> findAll() {
+    public List<People> findAll() 
+    {
+        
+        Query res = entityManager.createQuery("SELECT * FROM PEOPLE");
+        List<People> lRes = res.getResultList();
+        return lRes;
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<People> findByLogin(String login) 
+    {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
